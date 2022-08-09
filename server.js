9c8eef0589a76c1e941bbd19adfa05637970d89a
Remy.Aubert <r.aubert@ldlc.com>
2022-08-09 11:30:47 +0200

    edit

--- server.js
+++ server.js
@@ -6,15 +6,16 @@ const connection = mysql.createConnection({
   host     : process.env.HOST,
   user     : process.env.USER,
   password : process.env.PASSWORD
-}).then(async ()=> {
-  await connection.connect();
-  setInterval(async()=> {
-    await connection.query('SELECT 1', function(err, rows, fields) {
-      if (err) throw err;
-      console.log('The solution is: ', rows[0].solution);
-    });
-  },4000)
 })
+
+await connection.connect();
+setInterval(async()=> {
+  await connection.query('SELECT 1', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
+  });
+},4000)
+
 
 app.get('/', ()=> {
   console.log('get /')
